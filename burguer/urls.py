635ced7e74c3718f3ace668e834ace3fad7fbbca
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-08-11 16:07:53 -0400

    Update

--- burguer/urls.py
+++ burguer/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('produto/<int:burguer_id>', views.produto, name='produto'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
 
 
--- burguer/views.py
+++ burguer/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from .models import Produto
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    hamburguers = Produto.objects.all()
+    return render(request, 'home.html', {'hamburguers': hamburguers})
+
+
+def produto(request, burguer_id):
+    item = Produto.objects.get(pk=burguer_id)
+    return render(request, 'produto.html', {'item':item})
